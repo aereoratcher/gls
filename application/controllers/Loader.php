d5a2d179aaecfa10d7d9d4cd6fc38af34f3dd696
aereowatcher <luengo.ale@gmail.com>
2015-05-05 14:50:34 -0300

    doctrine basic impl, todavia con problemas pero consola funcional

--- application/controllers/Loader.php
+++ application/controllers/Loader.php
@@ -8,6 +8,17 @@ class Loader extends CI_Controller {
     }
 
     function index() {
+        $em = $this->doctrine->getEntityManager();
+
+
+        /*$this->load->model('usuarios');
+        $this->usuarios->setNombre('tara');
+
+        $em->persist($this->usuarios);
+        $em->flush();
+
+        echo "creado usuario con id ".$this->usuarios->getId();
+    */
         $this->layout->view('index');
     }
 }
